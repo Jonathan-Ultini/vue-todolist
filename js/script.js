15d536672b9038edd2b1c4e750329861259c0271
Jonathan-Ultini <jonathanultini@gmail.com>
2024-09-05 14:34:25 +0200

    scaletta

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-  console.log('Il DOM è completamente caricato e analizzato');
-});
+// milestone 1
+//1. creare struttura dati
+//2. visualizzare la lista
+//3. css per sbarrare testo
+
+//milestone 2
+//1. aggiungere il ulsante "X"
+//2. funzione di eliminazione
+
+//milestone 3
+//1. caampo input e pulsante inserimento
+//2. aggiungere nuovo todo
+
+//bonus
+//1. evento click al testo
+//2. invertire valore della proprietà al click
