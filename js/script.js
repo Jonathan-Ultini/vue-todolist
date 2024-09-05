bc7a895ceeeb9ec37c611dfa84f9bf8645aa3f5a
Jonathan-Ultini <jonathanultini@gmail.com>
2024-09-05 14:49:05 +0200

    milestone 1

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 // milestone 1
-//1. creare struttura dati
-//2. visualizzare la lista
-//3. css per sbarrare testo
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      // Creare la struttura dati
+      todos: [
+        { text: 'Studiare Vue.js', done: false },
+        { text: 'Fare esercizio di programmazione', done: false },
+        { text: 'Completare il progetto', done: true }
+      ]
+    };
+  }
+}).mount('#app');
 
 //milestone 2
 //1. aggiungere il ulsante "X"
 //2. funzione di eliminazione
 
 //milestone 3
-//1. caampo input e pulsante inserimento
+//1. campo input e pulsante inserimento
 //2. aggiungere nuovo todo
 
 //bonus
